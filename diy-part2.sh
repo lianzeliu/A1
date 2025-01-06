7d3b1aa8a75cb5bb867213cd1ea4ec5aba369930
zeliu <134698307+lianzeliu@users.noreply.github.com>
2025-01-06 11:42:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,1 +1,8 @@
+rm -rf package/feeds/packages/net/shadowsocks-rust
+rm -rf package/feeds/packages/net/mosdns
+sed -i 's/^PKG_VERSION:=.*/PKG_VERSION:=5.02.5187/' package/feeds/packages/softethervpn5/Makefile
+sed -i 's/^PKG_HASH:=.*/PKG_HASH:=skip/' package/feeds/packages/softethervpn5/Makefile
+sed -i 's/^PKG_VERSION:=.*/PKG_VERSION:=1.2.19/' package/feeds/packages/libtorrent-rasterbar/Makefile
+sed -i 's/^PKG_HASH:=.*/PKG_HASH:=skip/' package/feeds/packages/libtorrent-rasterbar/Makefile
+sed -i "s/\(hostname='\)[^']*\('.*\)/\1openwrt\2/" package/base-files/files/bin/config_generate
 
